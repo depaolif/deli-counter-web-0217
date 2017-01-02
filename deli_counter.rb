6474aa4d7253efc1a1fe8604429eb3aa1d627d81
harleqat <harleqat@gmail.com>
2017-01-01 19:41:23 -0500

    tested

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+def line(array)
+	if array.size < 1 
+			puts "The line is currently empty."
+	else
+		res = "The line is currently:"
+		array.each_with_index {|item,index| 
+			res += " #{index+1}. #{item}"
+		}
+		puts res
+	end
+end
+
+def take_a_number(array,string)
+	array.push(string)
+	puts "Welcome, #{string}. You are number #{array.size} in line."
+end
+
+def now_serving(array)
+	if array.size < 1
+		puts "There is nobody waiting to be served!"
+	else
+		puts "Currently serving #{array.shift}."
+	end
+end
